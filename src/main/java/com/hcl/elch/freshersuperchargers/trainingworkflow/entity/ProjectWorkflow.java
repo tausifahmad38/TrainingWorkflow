7b1072f2659f6a45b356b89205fa26178b09e4d1
tausifahmad38 <133738369+tausifahmad38@users.noreply.github.com>
2023-08-11 12:13:41 +0530

    Update ProjectWorkflow.java

--- src/main/java/com/hcl/elch/freshersuperchargers/trainingworkflow/entity/ProjectWorkflow.java
+++ src/main/java/com/hcl/elch/freshersuperchargers/trainingworkflow/entity/ProjectWorkflow.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
